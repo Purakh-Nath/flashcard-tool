33456b5b48dcf2195c861632d0cfde14405011d7
Purakh-Nath <purakhnath45@gmail.com>
2024-08-12 23:16:37 +0530

    fixed error

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,38 @@
-// import { defineConfig, loadEnv } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-  
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: `${import.meta.env.VITE_API_BASE_URL}`,
-//         changeOrigin: true,
-//         secure: false,
-//       }
-//     }
-//   },
-//   plugins: [react()],
-// })
-
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables based on the current mode ('development', 'production', etc.)
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_BASE_URL,
-          changeOrigin: true,
-          secure: false, 
-        }
+// https://vitejs.dev/config/
+export default defineConfig({
+  
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://flashcard-backend-ivory.vercel.app',
+        changeOrigin: true,
+        secure: false,
       }
-    },
-    plugins: [react()],
-  };
-});
+    }
+  },
+  plugins: [react()],
+})
+
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig(({ mode }) => {
+//   // Load environment variables based on the current mode ('development', 'production', etc.)
+//   const env = loadEnv(mode, process.cwd());
+
+//   return {
+//     server: {
+//       proxy: {
+//         '/api': {
+//           target: env.VITE_API_BASE_URL,
+//           changeOrigin: true,
+//           secure: false, 
+//         }
+//       }
+//     },
+//     plugins: [react()],
+//   };
+// });
